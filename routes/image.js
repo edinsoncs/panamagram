1e04e1aaa0d6506226a046336af610268399fe72
Edinson <jean_clude_dortosms@hotmail.com>
2016-04-15 02:57:25 -0300

    testing

--- routes/image.js
+++ routes/image.js
@@ -27,22 +27,40 @@ router.post('/', multipartMiddleware, function(req, res, next){
 			var directorio = path.join(__dirname, '..', 'public', 'imagenes/' + nameImagen);
 			var insertIMG = 'imagenes/' + nameImagen;
 			console.log(directorio);
-			fs.writeFile(directorio, data, function(err){
-				if(err) {
-					console.log(err);
-				}
-				else {
-					console.log(data);
-					basededatos.insert({
-						'Nombre': nameImagen,
-						'Url': insertIMG,
-						'Fecha': new Date()
-					}).success(function(doc){
-						res.redirect('http://somostodospanama.com/app/demo2/shared.html?id=' + doc._id);
-					}).error(function(err){
-						console.log(err);
+			require('lwip').open(data, function(err, image){
+
+
+				image.batch()
+				    .scale(0)          // scale to 75%
+				    .rotate(0)  // rotate 45degs clockwise (white fill)
+				    .crop(500, 500)       // crop a 200X200 square from center              // Gaussian blur with SD=5
+				    .writeFile(nameImagen, function(err){
+				      // check err...
+				      // done.
+				      console.log(image);
+
+				      fs.writeFile(directorio, image, function(err){
+						if(err) {
+							console.log(err);
+						}
+						else {
+							basededatos.insert({
+								'Nombre': nameImagen,
+								'Url': insertIMG,
+								'Fecha': new Date()
+							}).success(function(doc){
+								res.redirect('http://somostodospanama.com/app/demo2/shared.html?id=' + doc._id);
+							}).error(function(err){
+								console.log(err);
+							});
+						}
 					});
-				}
+
+				    });
+
+				
+				
+
 			});
 		}
 	});
