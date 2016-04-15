3f11876041fcf90b4337ed92978f054dbaa1f1c7
Edinson <jean_clude_dortosms@hotmail.com>
2016-04-15 03:04:52 -0300

    testing crop img

--- routes/image.js
+++ routes/image.js
@@ -6,6 +6,7 @@ var multipart = require('connect-multiparty');
 var path = require('path');
 
 var multipartMiddleware = multipart();
+var easyimg = require('easyimage');
 
 
 
@@ -24,43 +25,42 @@ router.post('/', multipartMiddleware, function(req, res, next){
 			console.log(err);
 		}
 		else {
+
+			easyimg.rescrop({
+			     src:nameImagen, dst: __dirname, '..', 'public', 'resizes/' + nameImagen,
+			     width:500, height:500,
+			     cropwidth:128, cropheight:128,
+			     x:0, y:0
+			  }).then(
+			  function(image) {
+			     console.log('Funciono ' + image.width + ' x ' + image.height);
+			  },
+			  function (err) {
+			    console.log(err);
+			  }
+			);
+
+
 			var directorio = path.join(__dirname, '..', 'public', 'imagenes/' + nameImagen);
 			var insertIMG = 'imagenes/' + nameImagen;
 			console.log(directorio);
-			require('lwip').open(data, function(err, image){
 
-
-				image.batch()
-				    .scale(0)          // scale to 75%
-				    .rotate(0)  // rotate 45degs clockwise (white fill)
-				    .crop(500, 500)       // crop a 200X200 square from center              // Gaussian blur with SD=5
-				    .writeFile(nameImagen, function(err){
-				      // check err...
-				      // done.
-				      console.log(image);
-
-				      fs.writeFile(directorio, image, function(err){
-						if(err) {
-							console.log(err);
-						}
-						else {
-							basededatos.insert({
-								'Nombre': nameImagen,
-								'Url': insertIMG,
-								'Fecha': new Date()
-							}).success(function(doc){
-								res.redirect('http://somostodospanama.com/app/demo2/shared.html?id=' + doc._id);
-							}).error(function(err){
-								console.log(err);
-							});
-						}
+			fs.writeFile(directorio, data, function(err){
+				if(err) {
+					console.log(err);
+				}
+				else {
+					console.log(data);
+					basededatos.insert({
+						'Nombre': nameImagen,
+						'Url': insertIMG,
+						'Fecha': new Date()
+					}).success(function(doc){
+						res.redirect('http://somostodospanama.com/app/demo2/shared.html?id=' + doc._id);
+					}).error(function(err){
+						console.log(err);
 					});
-
-				    });
-
-				
-				
-
+				}
 			});
 		}
 	});
